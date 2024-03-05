de7bc9516dc72475ce1bfd9df71902298f623b7e
s-ko <song.1209@icloud.com>
2024-03-05 23:22:50 +0900

    feat: テスト追加

--- backend/api/test_1_user.py
+++ backend/api/test_1_user.py
@@ -56,3 +56,56 @@ class UnauthorizedUserApiTests(TestCase):
         user.check_password(payload['password'])
       )
       self.assertNotIn('password', res.data)
+
+    def test_1_5_should_not_create_user_by_same_credentials(self):
+        payload = {'username': 'dummy', 'password': 'dummy_pw'}
+        get_user_model().objects.create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1_6_should_not_create_user_with_short_pw(self):
+        payload = {'username': 'dummy', 'password': 'pw'}
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1_7_should_response_token(self):
+        payload = {'username': 'dummy', 'password': 'dummy_pw'}
+        get_user_model().objects.create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_1_8_should_not_response_token_with_invalid_credentials(self):
+        get_user_model().objects.create_user(username='dummy', password='dummy_pw')
+        payload = {'username': 'dummy', 'password': 'wrong'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1_9_should_not_response_token_with_non_exist_credentials(self):
+        payload = {'username': 'dummy', 'password': 'dummy_pw'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1__10_should_not_response_token_with_missing_field(self):
+        #get_user_model().objects.create_user(username='dummy', password='dummy_pw')
+        payload = {'username': 'dummy', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1__11_should_not_response_token_with_missing_field(self):
+        #get_user_model().objects.create_user(username='dummy', password='dummy_pw')
+        payload = {'username': '', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_1__12_should_not_get_user_profile_when_unauthorized(self):
+        res = self.client.get(PROFILE_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
